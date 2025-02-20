853445b1334d3730e3d210278810db8a578caa7b
ufano.pro <ufano.pro@gmail.com>
2025-02-20 16:08:31 +0100

    seccion12 APP WEB

---
+++ 13-APP Web/gowiki/wiki.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"text/template"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Page{Title: title, Body: body}, nil
+}
+
+var validPath = regexp.MustCompile(`^/(edit|view|save)/([a-zA-Z0-9]+)$`)
+
+func makeHandler(fn func(w http.ResponseWriter, r *http.Request, title string)) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+
+		fn(w, r, m[2])
+	}	
+}
+
+
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplates(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplates(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+func renderTemplates(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	/*
+		p1 := &Page{Title: "TestPage", Body: []byte("Esta es una página de muestra")}
+		err := p1.save()
+		if err != nil {
+			fmt.Println("Error al guardar la página:", err)
+			return
+		}
+
+		p2, err := loadPage("TestPage")
+		if err != nil {
+			fmt.Println("Error al cargar la página:", err)
+			return
+		}
+
+		// Convertimos los bytes a string para imprimir correctamente el contenido
+		fmt.Println(string(p2.Body))
+	*/
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+	
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
